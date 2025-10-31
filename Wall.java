d30923a31afa0cd1fd1719579b25b87ce209e53f
aMOBSTERgail <349191643@gapps.yrdsb.ca>
2025-10-31 10:43:21 -0400

    Create Wall.java

---
+++ Wall.java
@@ -0,0 +1,13 @@
+# Welcome to our GitHub classroom!
+
+Do the following to Complete this assignment:
+
+1. Begin editing this file by clicking the 'pen' symbol above.
+
+2. Enter your First Name: Abigail
+
+3. Enter your favourite animal: Hamster
+
+4. Now click the green 'commit changes' button at the bottom.
+
+5. Done!
